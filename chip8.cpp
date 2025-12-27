1209bbc58f31f38f954bf78a480bc5876a9ea54c
nhantran1711 <traty191@mymail.unisa.edu.au>
2025-12-27 21:34:34 +1030

    Init SDL, starting the main method, final cleanup function

--- chip8.cpp
+++ chip8.cpp
@@ -2,7 +2,30 @@
 #include <iostream>
 #include <SDL3/SDL.h>
 
+// Init SDL
+bool init_sdl(void) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+        SDL_Log("Could not init SDL subsystem: %s" , SDL_GetError());
+        return false;
+    }
+    return true;
+}
+
+// Final clean up program
+void final_cleanup(void) {
+    SDL_Quit();
+}
+
+// Main method
 int main() {
-    
-    std::cout << "Testing on windows" << std::endl;
+
+    // Init SDL
+    if (!init_sdl()) {
+        exit(EXIT_FAILURE);
+    }
+
+    // Final Cleanup
+    final_cleanup();
+
+    exit(EXIT_SUCCESS);
 }
